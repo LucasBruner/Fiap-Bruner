ef754a4120c1a117e26249f9796905f9f3336780
Brenda Bernat <brendalouisebernat@gmail.com>
2025-10-21 13:32:53 -0300

    remove unused method

--- src/main/java/br/com/fiap/lunchtech/lunchtech/repositories/UsuarioRepository.java
+++ src/main/java/br/com/fiap/lunchtech/lunchtech/repositories/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package br.com.fiap.lunchtech.lunchtech.repositories;
 
-import br.com.fiap.lunchtech.lunchtech.dtos.UsuarioRequestDTO;
 import br.com.fiap.lunchtech.lunchtech.entities.Usuario;
 
 import java.util.List;
@@ -15,5 +14,4 @@ public interface UsuarioRepository {
     Integer updatePassword(String email, String newPassword);
     Optional<String> findUsername(String username);
     Optional<String> getPassword(String username);
-    Optional<String> getEmail(String email);
 }
