a95cb113ca893a1cd3be7f38af3d2b2058083692
Brenda Bernat <brendalouisebernat@gmail.com>
2025-10-21 16:47:39 -0300

    Documentação do Swagger

--- src/main/java/br/com/fiap/lunchtech/lunchtech/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/lunchtech/lunchtech/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import br.com.fiap.lunchtech.lunchtech.entities.Usuario;
 import br.com.fiap.lunchtech.lunchtech.services.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name = "Usuários", description = "Endpoints para gerenciamento de usuários")
 public class UsuarioController {
 
     private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
@@ -31,9 +34,9 @@ public class UsuarioController {
     }
 
     @Operation(
-            description = "Busca todos os usuários",
-            summary = "Busca todos os usuários",
-            responses = { @ApiResponse(description = "Ok", responseCode = "200")})
+        summary = "Busca todos os usuários",
+        description = "Busca todos os usuários. Deve-se informar a página e o tamanho da paginação para o retorno dos resultados. Retorna uma lista de JSON. Exemplo URL: http://localhost:8080/usuarios?page=1&size=10",
+        responses = { @ApiResponse(description = "Ok", responseCode = "200")})
     @GetMapping
     public ResponseEntity<List<Usuario>> findAllUsuarios(@RequestParam("page") Integer page,
                                                          @RequestParam("size") Integer size) {
@@ -43,9 +46,9 @@ public class UsuarioController {
     }
 
     @Operation(
-            description = "Busca os usuários por nome",
-            summary = "Busca de usuários por nome",
-            responses = { @ApiResponse(description = "Ok", responseCode = "200")})
+        summary = "Busca usuários por nome",
+        description = "Busca usuários por nome. Deve-se informar na URL um nome para o retorno dos resultados. Retorna uma lista de JSON. Exemplo URL: http://localhost:8080/usuarios/brenda",
+        responses = { @ApiResponse(description = "Ok", responseCode = "200")})
     @GetMapping("/{name}")
     public ResponseEntity<List<Usuario>> findUsuarioByName(@PathVariable("name") String name) {
         logger.info("GET -> /usuarios/name");
@@ -53,21 +56,33 @@ public class UsuarioController {
         return ResponseEntity.ok(usuario);
     }
 
+    @Operation(
+        summary = "Criação de novo usuário",
+        description = "Criação de novo usuário, onde são feitas as validações das regras dos campos e salva o novo usuário. Deve-se informar um JSON com as informações de usuário.",
+        responses = { @ApiResponse(description = "Created", responseCode = "201"), @ApiResponse(description = "Bad request", responseCode = "400")})
     @PostMapping
-    public ResponseEntity<Void> saveUsuario(@RequestBody Usuario usuario) {
+    public ResponseEntity<Void> saveUsuario(@Valid @RequestBody Usuario usuario) {
         logger.info("POST -> /usuarios");
         this.usuarioService.saveUsuario(usuario);
         return ResponseEntity.status(201).build();
     }
 
+    @Operation(
+        summary = "Alteração de informações do usuário",
+        description = "Alteração de informações do usuário, onde são feitas as validações das regras dos campos e atualiza as informações do usuário. Deve-se informar um JSON com as informações de usuário e na URL informar o email do usuário que será alterado. A data de alteração será atualizada automaticamente com a data atual do sistema.",
+        responses = { @ApiResponse(description = "Ok", responseCode = "200"), @ApiResponse(description = "Bad request", responseCode = "400")})
     @PutMapping("/{email}")
-    public ResponseEntity<Void> updateUsuario(@RequestBody Usuario usuario,
+    public ResponseEntity<Void> updateUsuario(@Valid @RequestBody Usuario usuario,
                                               @PathVariable("email") String email) {
         logger.info("PUT -> /usuarios");
         this.usuarioService.updateUsuario(usuario, email);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+        summary = "Exclusão de usuário",
+        description = "Exclusão de usuário. Deve-se informar o email do usuário que será excluído. Exemplo: http://localhost:8080/usuarios/delete?email=joao@fiap.com",
+        responses = { @ApiResponse(description = "Ok", responseCode = "200"), @ApiResponse(description = "Not found", responseCode = "404")})
     @DeleteMapping("/delete")
     public ResponseEntity<Void> deleteUsuario(@RequestParam("email") String email) {
         logger.info("DELETE -> /usuarios");
@@ -75,6 +90,10 @@ public class UsuarioController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+        summary = "Alteração da senha do usuário",
+        description = "Alteração da senha do usuário. Deve-se informar o email do usuário que será alterado e a nova senha. A senha não pode ser vazia ou possuir apenas espaços. A data de alteração será atualizada automaticamente com a data atual do sistema. Exemplo: http://localhost:8080/usuarios/change-password/joao@fiap.com?password=123",
+        responses = { @ApiResponse(description = "Ok", responseCode = "200"), @ApiResponse(description = "Not found", responseCode = "404")})
     @PutMapping("/change-password/{email}")
     public ResponseEntity<Void> changePassword(@PathVariable("email") String email,
                                                @RequestParam("password") String password) {
