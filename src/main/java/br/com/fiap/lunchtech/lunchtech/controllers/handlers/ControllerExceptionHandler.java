784210b3c3c8565b4d6c97a78dd97a13b0609cfb
Brenda <68514825+blbernat@users.noreply.github.com>
2025-10-29 21:43:02 -0300

    Use ProblemDetail to handle exceptions (#14)

--- src/main/java/br/com/fiap/lunchtech/lunchtech/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/lunchtech/lunchtech/controllers/handlers/ControllerExceptionHandler.java
@@ -1,19 +1,16 @@
 package br.com.fiap.lunchtech.lunchtech.controllers.handlers;
 
-import br.com.fiap.lunchtech.lunchtech.dtos.CreateUserDTO;
-import br.com.fiap.lunchtech.lunchtech.dtos.InvalidLoginDTO;
-import br.com.fiap.lunchtech.lunchtech.dtos.MethodArgumentDTO;
-import br.com.fiap.lunchtech.lunchtech.dtos.ResourceNotFoundDTO;
 import br.com.fiap.lunchtech.lunchtech.services.exceptions.CreateUserException;
 import br.com.fiap.lunchtech.lunchtech.services.exceptions.InvalidLoginException;
 import br.com.fiap.lunchtech.lunchtech.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,30 +18,46 @@ import java.util.List;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ResourceNotFoundDTO> handlerResourceNotFoundException(ResourceNotFoundException e) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
-        return ResponseEntity.status(status.value()).body(new ResourceNotFoundDTO(e.getMessage(), status.value()));
+    public ProblemDetail handlerResourceNotFoundException(ResourceNotFoundException e) {
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problem.setTitle("Erro ao salvar usuário!");
+        problem.setDetail(e.getMessage());
+        problem.setType(URI.create("http://localhost:8080/v1/usuarios"));
+        return problem;
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<MethodArgumentDTO> handlerResourceNotFoundException(MethodArgumentNotValidException e) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+    public ProblemDetail handlerResourceNotFoundException(MethodArgumentNotValidException e) {
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problem.setTitle("Erro ao salvar usuário!");
+        problem.setDetail(e.getTitleMessageCode());
+        problem.setType(URI.create("http://localhost:8080/v1/usuarios"));
+
         List<String> errors = new ArrayList<String>();
         for (FieldError error : e.getBindingResult().getFieldErrors()) {
             errors.add(error.getField() + ": " + error.getDefaultMessage());
         }
-        return ResponseEntity.status(status.value()).body(new MethodArgumentDTO(errors, status.value()));
+
+        problem.setProperty("errors", errors);
+
+        return problem;
     }
 
     @ExceptionHandler(InvalidLoginException.class)
-    public ResponseEntity<InvalidLoginDTO> handlerInvalidLoginException(InvalidLoginException e) {
-        HttpStatus status = HttpStatus.UNAUTHORIZED;
-        return ResponseEntity.status(status.value()).body(new InvalidLoginDTO(e.getMessage(), status.value()));
+    public ProblemDetail handlerInvalidLoginException(InvalidLoginException e) {
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
+        problem.setTitle("Erro ao realizar login!");
+        problem.setDetail(e.getMessage());
+        problem.setType(URI.create("http://localhost:8080/v1/login"));
+        return problem;
     }
 
     @ExceptionHandler(CreateUserException.class)
-    public ResponseEntity<CreateUserDTO> handlerCreateUserException(CreateUserException e) {
-        HttpStatus status = HttpStatus.METHOD_NOT_ALLOWED;
-        return ResponseEntity.status(status.value()).body(new CreateUserDTO(e.getMessage(), status.value()));
+    public ProblemDetail handlerCreateUserException(CreateUserException e) {
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.METHOD_NOT_ALLOWED);
+        problem.setTitle("Erro ao salvar usuário!");
+        problem.setDetail(e.getMessage());
+        problem.setType(URI.create("http://localhost:8080/v1/usuarios"));
+        return problem;
     }
 }
